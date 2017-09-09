71b7ac2d40b8cd4ea363320cd99fdb086d5e8de8
Stephan Schröder <simon.void+git@gmail.com>
2017-09-09 23:15:55 +0200

    add helper methods: setDate as milliseconds on pikaday setMinDate as milliseconds on pikaday setMaxDate as milliseconds on pikaday

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,15 @@
 # Changelog
 
+## 1.2.0
+
+- add helper method: setDate as milliseconds on pikaday 
+- add helper method: setMinDate as milliseconds on pikaday 
+- add helper method: setMaxDate as milliseconds on pikaday 
+
+## 1.1.0
+
+- add helper method: get milliseconds from pikaday
+
 ## 1.0.0
 
 - Initial version
--- lib/pikaday_dart_helpers.dart
+++ lib/pikaday_dart_helpers.dart
@@ -8,3 +8,23 @@ import 'package:pikaday/pikaday.dart';
 /// supported in package:js.
 @JS()
 external int getPikadayMillisecondsSinceEpoch(Pikaday pikaday);
+
+/// A helper function for calling
+/// pikaday.setDate(new Date(millies), dontTriggerOnSelect)
+/// since DateTimes are not yet supported in package:js.
+/// If triggerOnSelect is not set to true, it will default to false.
+@JS()
+external void setPikadayMillisecondsSinceEpoch(
+    Pikaday pikaday, int millies, [bool triggerOnSelect]);
+
+/// A helper function for calling pikaday.setMinDate(new Date(millies))
+/// since DateTimes are not yet supported in package:js.
+@JS()
+external void setPikadayMinDateAsMillisecondsSinceEpoch(
+    Pikaday pikaday, int millies);
+
+/// A helper function for calling pikaday.setMaxDate(new Date(millies))
+/// since DateTimes are not yet supported in package:js.
+@JS()
+external void setPikadayMaxDateAsMillisecondsSinceEpoch(
+    Pikaday pikaday, int millies);
